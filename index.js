409cebf386cc8c2de196705f91560b73f0e13138
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-11 18:19:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ function johnLennonFacts(facts){var newFacts = []; var n = 0;
   while(n<facts.length){newFacts.push(`${facts[n]}!!!`); n++} return newFacts}
 
 function iLoveTheBeatles(n){var newArr = []; var j = 0;
-do{j++; newArr.push('I love the Beatles!');} while(j<15); return newArr}
+do{j++; n = newArr.push('I love the Beatles!');} while(j<15); return newArr}
