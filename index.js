97f13654903504d08dcf0c9c57d59002eaa94067
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-11 17:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // add solution here
 var empty = []
-var musicians = ['John Lennon', 'Paul McCartney', 'Ringo Starr']
+var musicians = ['John Lennon', 'Paul McCartney', 'Ringo Starr', 'George Harrison']
+var instruments = ['guitar', 'bass', 'drums', 'lead guitar']
+for (var i = 0; i<musicians.length; i++){
+  
+}
