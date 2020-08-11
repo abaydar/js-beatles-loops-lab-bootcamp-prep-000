8324b23af30fc4c144b3b75fe8004577b66129a8
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-11 17:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var musicians = ['John Lennon', 'Paul McCartney', 'Ringo Starr', 'George Harriso
 var instruments = ['guitar', 'bass', 'drums', 'lead guitar']
 function theBeatlesPlay(musicians, instruments){
 for (var i = 0; i<4; i++){}
-  empty.push(musicians.i + " plays the " + instruments.i); return empty
+  empty.push(`${musicians[i]} plays the ${instruments[i]}`); return empty
 }
