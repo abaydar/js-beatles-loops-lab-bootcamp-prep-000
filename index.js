1bc7ab227aa6f63b8fab6c7c3eaeb27c51104fbf
bold-chain-4657 <amandabaydar@gmail.com>
2020-08-11 17:44:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ for (var i = 0; i<4; i++){empty.push(`${musicians[i]} plays ${instruments[i]}`)}
  return empty
 }
 
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
